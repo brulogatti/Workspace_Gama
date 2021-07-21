2ab9e6dc6fb7b557bcb29e075b999acfb8c0bc4b
brulogatti <bru.logatti@gmail.com>
2021-07-21 15:08:03 -0300

    21072021

---
+++ GraphQL/Projeto_aula/packages/server/src/main.js
@@ -0,0 +1,34 @@
+import express, { response } from 'express';
+import cors from 'cors';
+
+const server = express();
+
+server.get('/status', (_, response) => {
+    response.send({
+        status: "OK",
+    });
+}
+); 
+
+const enableCors = cors({origin: 'http://localhost:3000'});
+
+server
+.options('/authenticate', enableCors)
+.post('/authenticate', enableCors, express.json(), (request, response) => {
+    console.log(
+        'E-mail', request.body.email,
+        'Senha', request.body.password
+    );
+    response.send({
+        Okay: true,
+    }
+    )
+});
+
+
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 8000;
+const HOSTNAME = process.env.HOSTNAME || '127.0.0.1'; 
+
+server.listen(PORT, HOSTNAME, () => {
+    console.log(`Server is listening at http://${HOSTNAME}:${PORT}`);
+});
