d31b3b386b6f616e852606906dbda146756e5593
brulogatti <bru.logatti@gmail.com>
2021-07-24 16:00:00 -0300

    24072021

--- GraphQL/Projeto_aula/packages/server/src/graphql/resolvers.js
+++ GraphQL/Projeto_aula/packages/server/src/graphql/resolvers.js
@@ -1,15 +1,21 @@
-import { resolvers as demandsResolvers } from "./Demand/Demand";
+import { resolvers as nodeResolvers } from "./Node/Node";
+import {resolvers as listResolvers} from "./List/List";
 import {resolvers as clientResolvers} from "./Client/Client";
+import { resolvers as demandsResolvers } from "./Demand/Demand";
+
 
 const resolvers = {
-    ...demandsResolvers,
+
+    ...nodeResolvers,
+    ...listResolvers,
     ...clientResolvers,
+    ...demandsResolvers,
 
     Query:{
-        ...demandsResolvers.Query,
         ...clientResolvers.Query,
+        ...demandsResolvers.Query,
     },
-}
+};
 
 export default resolvers;
 
